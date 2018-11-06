5f7b889bc3872fdb4da489d68c547dd9a27c785f
William Brown <wrbrown3530@gmail.com>
2018-11-06 14:54:11 -0700

    added a feature where the user's last search will be displayed on the page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const button = document.getElementsByClassName("searchButton")
   const userText = document.getElementsByClassName("searchInput")
   const output = document.getElementById("output")
+  const searchOutput =document.getElementById("searchOutput")
 
   button[0].addEventListener("click", function() {
 
@@ -13,7 +14,9 @@ document.addEventListener('DOMContentLoaded', function() {
           .then(function(response) {
             output.innerHTML = userText[0].value + " is from the planet " + response.data.name
             localStorage.setItem("history", userText[0].value)
-          })
+            let searchHistory = localStorage.getItem("history")
+             searchOutput.innerHTML = searchHistory + " is the last character that you searched for"
+            })
 
           .catch(function(error) {
 
@@ -27,22 +30,21 @@ document.addEventListener('DOMContentLoaded', function() {
           output.innerHTML = "The Jedi Temple Archives show no record of that character"
         } else if (num === 2) {
           output.innerHTML = "Everytime you search a character that doesn't exist, Anakin kills a youngling"
-        } else  {
+        } else {
           output.innerHTML = "It's treason then"
         }
       })
   })
-
 
   const emailSubmit = document.getElementsByClassName("myForm")
   const emailField = document.getElementsByClassName("email")
   emailSubmit[0].addEventListener("submit", function(event) {
     localStorage.setItem("email", emailField[0].value)
 
-    //console.log(emailField[0].value)
+
+
 
   })
-
 
 
 
